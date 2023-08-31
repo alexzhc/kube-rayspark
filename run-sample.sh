567b1215c3b6a0f856d1248183cbec50691efc8a
alexzhc <alex.zheng@daocloud.io>
2023-08-31 23:40:47 +0800

    added ingress
    Signed-off-by: alexzhc <alex.zheng@daocloud.io>

--- run-sample.sh
+++ run-sample.sh
@@ -12,6 +12,7 @@ kubectl create cm ${job_name} --from-file=./
 cd ../ || exit 1
 
 kubectl delete rayjob ${job_name}
+kubectl delete ingress ${job_name}
 cat ray-job.yaml \
     | yq '.metadata.name = env(job_name)' \
     | yq '.spec.entrypoint = "python /home/ray/samples/" + env(job_file)' \
@@ -19,6 +20,17 @@ cat ray-job.yaml \
     | yq '.spec.rayClusterSpec.workerGroupSpecs[0].template.spec.volumes[0].configMap.name = env(job_name) ' \
     | kubectl apply -f -
 
+# Add ingress
+cluster_name=$( kubectl get rayjob ${job_name} -o yaml | yq .status.rayClusterName )
+export cluster_name
+cat ray-ingress.yaml \
+    | yq '.metadata.name = env(job_name)' \
+    | yq '.spec.rules[0].http.paths[0].backend.service.name = env(cluster_name) + "-head-svc"' \
+    | kubectl apply -f -
+
+open http://ray.k8s.local/
+
+# Wait until finish
 until kubectl get job/${job_name}; do
     sleep 1
 done
